55f8f11778dabd602d2ccdc89cc622c27dd2fae7
blaeugh <50417843+blaeugh@users.noreply.github.com>
2019-11-26 11:44:50 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function flip(){
+    document.getElementById("top").onclick;
+    if (Math.ceil(Math.random() * 2) % 2 == 0){
+        show("heads");
+    }
+    else{
+        show("tails");
+    }
+}
+
+function show(){
+    document.getElementById("result").innerHtml;
+}
+
+flip();
+
+console.log(show)
