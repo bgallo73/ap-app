d12620a4c6696e720e11993fc470e5bf3f88d2c4
blaeugh <50417843+blaeugh@users.noreply.github.com>
2019-11-27 11:08:41 -0500

    images

--- app.js
+++ app.js
@@ -12,4 +12,3 @@ function flip(){
 function show(coin) {
     document.getElementById("result").innerHTML = coin; // this took me an embarrassingly long time to do, but thanks anyway https://stackoverflow.com/questions/32302066/coin-toss-with-javascript-and-html
 }
-
