e2f85efe55f69be63b043e3a8e67257885b9c5aa
brendangallo <50417843+bgallo73@users.noreply.github.com>
2019-12-05 21:04:59 -0500

    finished flip and coin part, beginning to add images

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-document.getElementById("flip-coin").onclick = function(){
-    if (Math.ceil(Math.random() * 2) % 2 == 0) {
-        show("heads");
+document.getElementById("flip-coin").onclick = function coinFlip(){
+    let coinValue = (Math.ceil(Math.random() * 2) % 2)
+    if (coinValue == 0){
+        document.getElementById("coin-result").innerHTML = "heads";
+        document.getElementById("coin-image").src = "img/heads.png";
     }
     else{
-        show("tails");
+        document.getElementById("coin-result").innerHTML = "tails";
+        document.getElementById("coin-image").src = "img/tails.png";
     }
 }
 
-function show(coin) {
-    document.getElementById("coin-result").innerHTML = coin; // this took me an embarrassingly long time to do, but thanks anyway https://stackoverflow.com/questions/32302066/coin-toss-with-javascript-and-html
+document.getElementById("roll-dice").onclick = function diceRoll() {
+    let die1 = (Math.floor(Math.random() * 6) + 1)
+    let die2 = (Math.floor(Math.random() * 6) + 1)
+    document.getElementById("die-1-result").innerHTML = die1;
+    document.getElementById("die-2-result").innerHTML = die2;
 }
 
-document.getElementById("roll-dice").onclick = function diceRoll(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-    
-}
-
-function show(dice) {
-    document.getElementById("dice-result").innerHTML = dice;
-}
