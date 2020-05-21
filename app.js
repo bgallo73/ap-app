b31013cc7e00b49b63ee4982677f7d8b1577e406
brendangallo <50417843+bgallo73@users.noreply.github.com>
2020-05-20 21:40:33 -0400

    xxs

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-document.getElementById("flip-coin").onclick = function coinFlip(){
-    let amount = prompt("How many times would you like to flip the coin?")
-    while (amount < 1|| amount > 10000){
-        amount = prompt("How many times would you like to flip the coin? Any value from 1 to 10000.")
+let byId = document.getElementById.bind(document)
+
+byId("flip-coin").onclick = function coinClick(){
+    let amount = parseInt(byId("coin-amount").value)
+    if (amount < 1|| amount > 10000){
+        alert("Choose a number from 1 to 1000.")
     }
     let heads = 0;
     let tails = 0;
@@ -13,34 +15,22 @@ document.getElementById("flip-coin").onclick = function coinFlip(){
             tails++
         }
     }
-    console.log(heads)
-    console.log(tails)
-    document.getElementById("heads-amount-p").classList.remove("hidden");
-    document.getElementById("heads-amount").innerHTML = heads;
-    document.getElementById("tails-amount-p").classList.remove("hidden");
-    document.getElementById("tails-amount").innerHTML = tails;
+    byId("heads-amount-p").classList.remove("hidden");
+    byId("heads-amount").innerHTML = heads;
+    byId("tails-amount-p").classList.remove("hidden");
+    byId("tails-amount").innerHTML = tails;
     if(heads > tails){
-        document.getElementById("coin-image").src = "img/heads.png"
+        byId("coin-image").src = "img/heads.png"
     }
     else{
-        document.getElementById("coin-image").src = "img/tails.png"
+        byId("coin-image").src = "img/tails.png"
     }
 }
 
-document.getElementById("roll-dice").onclick = function diceRoll() {
-    /* let die1 = (Math.floor(Math.random() * 6) + 1);
-    let die2 = (Math.floor(Math.random() * 6) + 1);
-    document.getElementById("die-1-result").innerHTML = die1;
-    document.getElementById("die-2-result").innerHTML = die2;
-    document.getElementById("die-1-img").src = "img/dice-" + die1 + ".png";
-    document.getElementById("die-2-img").src = "img/dice-" + die2 + ".png";
-    document.getElementById("dice-result-p").classList.remove("hidden");
-    document.getElementById("dice-score-message").classList.remove("hidden");
-    document.getElementById("dice-score").innerHTML = score; */
-
-    let amount = prompt("How many dice would you like to roll the dice? Minimum of 1, maximum of 5.")
-    while (amount > 5 || amount < 1){
-        amount = prompt("How many dice would you like to roll the dice? Minimum of 1, maximum of 5.")
+byId("roll-dice").onclick = function diceRoll() {
+    let amount = parseInt(byId("dice-amount").value)
+    if (amount < 1|| amount > 5){
+        alert("Choose a number from 1 to 5.")
     }
 
     let rollResults = [];
@@ -50,16 +40,17 @@ document.getElementById("roll-dice").onclick = function diceRoll() {
     diceSum = rollResults.reduce(function(rollResults, a) { return rollResults + a; }, 0);
     diceAverage = (diceSum / amount);
 
-    document.getElementById("dice-result-p").classList.remove("hidden");
-    document.getElementById("dice-result").innerHTML = rollResults;
-    console.log(rollResults)
-    /* document.getElementById("die-" + rollResults[0] + "-img").src = "img/dice-" + rollResults[0] + ".png";
-    document.getElementById("die-" + rollResults[rollResults.length - 1] + "-img").src = "img/dice-" + rollResults[rollResults.length - 1] + ".png"; */
-    console.log(diceSum)
+    byId("dice-result-p").classList.remove("hidden");
+    byId("dice-result").innerHTML = rollResults;
+    byId("die-1-img").src = "img/dice-" + rollResults[0] + ".png"
+    byId("die-2-img").src = "img/dice-" + rollResults[1] + ".png"
+    byId("die-3-img").src = "img/dice-" + rollResults[2] + ".png"
+    byId("die-4-img").src = "img/dice-" + rollResults[3] + ".png"
+    byId("die-5-img").src = "img/dice-" + rollResults[4] + ".png"
     if (amount > 1){ //if statement so total and average are ignored if only one die is rolled
-    document.getElementById("dice-sum-p").classList.remove("hidden")
-    document.getElementById("dice-sum").innerHTML = diceSum;
-    document.getElementById("dice-average-p").classList.remove('hidden')
-    document.getElementById("dice-average").innerHTML = diceAverage;
+        byId("dice-sum-p").classList.remove("hidden")
+        byId("dice-sum").innerHTML = diceSum;
+        byId("dice-average-p").classList.remove('hidden')
+        byId("dice-average").innerHTML = diceAverage;
     }
     }
